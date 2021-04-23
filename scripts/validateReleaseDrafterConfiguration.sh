721b585c2a62fda25f4a49b4346c527b7c7a0f34
Pierre Beitz <pibeitz@gmail.com>
2021-04-23 12:25:42 +0200

    Use the official release drafter url

--- scripts/validateReleaseDrafterConfiguration.sh
+++ scripts/validateReleaseDrafterConfiguration.sh
@@ -11,9 +11,8 @@ pushd $TMPDIR || exit
 echo "Installing AJV"
 npm install -s -g "ajv-cli@${AJV_VERSION}"
 
-# TODO the schema should come from json schema once https://github.com/release-drafter/release-drafter/pull/600 is fixed
 echo "Downloading the release drafter validation schema"
-curl -Lso schema.json 'https://raw.githubusercontent.com/release-drafter/release-drafter/cec90728b088fe0fbc42bc15dd3d21bc59db34c6/schema.json'
+curl -Lso schema.json 'https://raw.githubusercontent.com/release-drafter/release-drafter/master/schema.json'
 
 echo "Validating release drafter configuration"
 ajv validate -s schema.json -d "${RELEASE_DRAFTER_CONFIG}"
